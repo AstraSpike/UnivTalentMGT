679519d4fa7e782d4e31c6fe533cdd89da49ba7b
AstraSpike <2388569504@qq.com>
2025-06-24 18:18:37 +0800

    1.2

--- hm-gateway/src/main/java/com/hmall/gateway/config/CorsConfig.java
+++ hm-gateway/src/main/java/com/hmall/gateway/config/CorsConfig.java
@@ -24,20 +24,20 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         // 允许所有域名进行跨域调用
         config.addAllowedOriginPattern("*");
-        // 允许跨域发送cookie
-        config.setAllowCredentials(true);
+        //config.addAllowedOrigin("http://localhost:5173");
         // 放行全部原始头信息
         config.addAllowedHeader("*");
         // 允许所有请求方法跨域调用
         config.addAllowedMethod("*");
         // 暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
         config.addExposedHeader("*");
+        // 允许跨域发送cookie
+        config.setAllowCredentials(true);
         // 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return new CorsWebFilter(source);
     }
 }
